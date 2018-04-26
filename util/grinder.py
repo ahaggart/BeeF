9e65f9e8310ed8527acecd74b4b03d628675995d
ahaggart <alex.haggart@gmail.com>
2018-04-26 16:55:38 -0700

    changed grinder util to do trivial optimizations, deprecated old .cow files

--- util/grinder.py
+++ util/grinder.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python2.7
 """
     compress compiled vm assembly from butcher into pure BeeF assembly
+
+    also does some late optimization
 """
 import sys
 
 ASSEMBLY_CHARS = {'_','^','>','<','+','-','[',']'}
+COMPLEMENT = {
+    '+':'-',
+    '-':'+',
+    '>':'<',
+    '<':'>',
+    '_':'^', # ebut not the other way around
+}
 COMMENT_DELIM = '#'
 LINE_MAX = 80
+NEWLINE = '\n'
+
+def chunks(_list, chunk_size):
+    for i in xrange(0, len(_list), chunk_size):
+        yield _list[i:i + chunk_size]
 
 def main():
     if len(sys.argv) != 3:
@@ -15,7 +29,7 @@ def main():
     with open(sys.argv[1],'r') as src:
         with open(sys.argv[2],'w') as dest:
             in_directives = True
-            char_count = 0
+            line = []
             while True:
                 char = src.read(1)
                 if not char:
@@ -23,11 +37,16 @@ def main():
                 if char == COMMENT_DELIM and in_directives:
                     src.readline() # throw out the rest of the line
                 elif char in ASSEMBLY_CHARS:
-                    if char_count == LINE_MAX:
-                        dest.write('\n')
-                        char_count = 0
-                    dest.write(char)
-                    char_count = char_count + 1
+                    # remove cancelling instructions
+                    if line and char in COMPLEMENT and line[-1] == COMPLEMENT[char]:
+                        line.pop()
+                    else:
+                        line.append(char)
+
+            lines = []
+            for chunk in chunks(line,LINE_MAX):
+                lines.append(str.join("",chunk))
+            dest.write(str.join("\n",lines))
             
 
 if __name__ == '__main__':
