03410fd873eeead8747faa3a3fa39fc5af6cc081
ahaggart <alex.haggart@gmail.com>
2018-04-28 12:12:36 -0700

    json format for CFG, first() and nullable() working

--- lang/parse.py
+++ lang/parse.py
@@ -5,23 +5,55 @@
 from __future__ import print_function
 import sys
 import pprint as pp
+import json
+
+TERMINALS_KEY = "terminals"
+GENERICS_KEY  = "generics"
+RULES_KEY     = "rules"
+START_KEY     = "start"
 
 class Grammar:
-    def __init__(self,nonterminals,terminals,rules,start):
+    def __init__(self,nonterminals,rules,start):
         self.start          = start
-        self.nonterminals   = set(nonterminals)
-        self.terminals      = set(terminals)
+        self.nonterminals   = dict([(nont.symbol,nont) for nont in nonterminals])
+#        self.terminals      = terminals
         self.rules          = dict([(rule.symbol,rule) for rule in rules])
         self.start          = start
 
-        if not self.start in self.nonterminals:
+        if not self.start.symbol in self.nonterminals:
             parse_error("Invalid start variable: {}".format(self.start))
+
+    @staticmethod
+    def create(source):
+        # TODO: do the class packing in the classes themselves?
+        terminals   = set(Terminal(term) for term in source[TERMINALS_KEY])
+        generics    = set(source[GENERICS_KEY])
+        full_syms = set(source[TERMINALS_KEY])
+        full_syms.update(generics)
+        rules,nt    = Rule.create(source[RULES_KEY],full_syms)
+        nonterminals= [ Nonterminal(sym) for sym in nt ]
+        start       = Nonterminal(source[START_KEY])
+        return Grammar(nonterminals,rules,start)
+
 
 class Derivation:
     def __init__(self,symbols):
         self.symbol = None
         self.symbols = symbols
         self.is_nullable = None
+        self.first_symbol = None
+
+    @staticmethod
+    def create(source,terminals,nonterminals):
+        tokens = []
+        for symbol in str.split(str(source)," "):
+            if symbol in terminals:
+                tokens.append(Terminal(symbol))
+            elif symbol in nonterminals:
+                tokens.append(Nonterminal(symbol))
+            else:
+                parse_error("Unspecified symbol in rule: {}".format(symbol))
+        return Derivation(tokens)
 
     def nullable(self,rules,pending):
         if self.is_nullable == None:
@@ -32,11 +64,21 @@ class Derivation:
             if symbol.symbol not in pending:
                 yield symbol.nullable(rules,pending)
 
+    def first(self,rules,pending):
+        if self.first_symbol == None:
+            self.first_symbol = set()
+            for symbol in self.symbols:
+                if not symbol.nullable(rules,set()):
+                    self.first_symbol.update(symbol.first(rules,pending))
+                    break
+                self.first_symbol.update(symbol.first(rules,pending))
+        return self.first_symbol
 
 class Nonterminal:
     def __init__(self,symbol):
         self.symbol = symbol
         self.is_nullable = None
+        self.first_symbol = None   
 
     def nullable(self,rules,pending):
         if self.is_nullable == None:
@@ -46,82 +88,132 @@ class Nonterminal:
             self.is_nullable = any(rules[self.symbol]._nullables(rules,pending))
         return self.is_nullable
 
+    def first(self,rules,pending):
+        if self.first_symbol == None:
+            if self.symbol not in rules:
+                parse_error("No rule for nonterminal: " + self.symbol)
+            self.first_symbol = set()
+            pending.add(self.symbol)
+            self.first_symbol.update(rules[self.symbol].first(rules,pending))
+        return self.first_symbol
+            
+
 class Rule:
     def __init__(self,symbol,derivations):
         self.symbol      = symbol
         self.derivations = derivations
         for derivation in self.derivations:
             derivation.symbol = self.symbol
+
+    @staticmethod
+    def create(source,terminals):
+        # expect a dict of nonterm->term
+        # we will build the symbol sets for nonterms
+        nonterms = set([symbol for symbol in source])
+        rules = []
+        for symbol in source:
+            ders = []
+            for derivation in source[symbol]:
+                if derivation == None:
+                    ders.append(Epsilon())
+                    continue
+                ders.append(Derivation.create(derivation,terminals,nonterms))
+            rules.append(Rule(symbol,ders))
+        return rules,nonterms
+
+
+        
+
     def _nullables(self,rules,pending):
         if not self.derivations:
             parse_error("Nonterminal has no derivations: "+self.symbols)
         for derivation in self.derivations:
             yield derivation.nullable(rules,pending)
 
+    def first(self,rules,pending):
+        first = set()
+        for derivation in self.derivations:
+            first.update(derivation.first(rules,pending))
+        return first
+
 class Terminal:
     def __init__(self,symbol):
         self.symbol = symbol
     def nullable(self,rules,pending):
         return False
+    def first(self,rules,pending):
+        return set([self.symbol])
 
 class Epsilon: # we might not need this
     def nullable(self,rules,pending):
         return True
+    def first(self,rules,pending):
+        return set()
 
 # produce a dictionary(variable->terminal) of the first terminals in strings
 # generated by each variable
-def first(rules):
-    pass
+def first(grammar):
+    firsts = dict()
+    for nt in grammar.nonterminals:
+        nonterm = grammar.nonterminals[nt]
+        firsts[nonterm.symbol] = nonterm.first(grammar.rules,set()) 
+    return firsts
 
 # produce a set(variable) of variables that can generate the empty string
 # walk through derivations, using the start variable as entry point
 def nullable(grammar):
     nullables = set()
-    for nonterm in grammar.nonterminals:
+    for nt in grammar.nonterminals:
+        nonterm = grammar.nonterminals[nt]
         if nonterm.nullable(grammar.rules,set()):
             nullables.add(nonterm.symbol)
     return nullables
             
-    
-
 
 def follow(rules):
     pass
 
 def main():
-    start = Nonterminal("start")
-    var1  = Nonterminal("var1")
-    var2  = Nonterminal("var2")
+    # start = Nonterminal("start")
+    # var1  = Nonterminal("var1")
+    # var2  = Nonterminal("var2")
 
-    epsilon = Epsilon()
+    # epsilon = Epsilon()
 
-    st = Terminal("s")
-    t1 = Terminal("t1")
-    t2 = Terminal("t2")
+    # st = Terminal("s")
+    # t1 = Terminal("t1")
+    # t2 = Terminal("t2")
 
-    rstart = Rule(start.symbol,[
-        Derivation([var1,var2]),
-        Derivation([st]),
-    ])
+    # rstart = Rule(start.symbol,[
+    #     Derivation([var1,var2]),
+    #     Derivation([st]),
+    # ])
 
-    rvar1 = Rule(var1.symbol,[
-        Derivation([var2,t2]),
-        Derivation([t2]),
-    ])
+    # rvar1 = Rule(var1.symbol,[
+    #     Derivation([var2,t2]),
+    #     Derivation([t2]),
+    # ])
 
-    rvar2 = Rule(var2.symbol,[
-        Derivation([var2,t1]),
-        epsilon,
-    ])
+    # rvar2 = Rule(var2.symbol,[
+    #     Derivation([var2,t1]),
+    #     epsilon,
+    # ])
 
-    g = Grammar(
-        [start,var1,var2],
-        [st,t1,t2],
-        [rstart,rvar1,rvar2],
-        start
-    )
+    # g = Grammar(
+    #     [start,var1,var2],
+    #     [st,t1,t2],
+    #     [rstart,rvar1,rvar2],
+    #     start
+    # )
 
+    with open("cow.json","r") as src:
+        g = Grammar.create(json.loads(src.read()))
+
+    print("NULLABLES:")
     pp.pprint(nullable(g))
+
+    print("STARTS:")
+    pp.pprint(first(g))
 
 def parse_error(reason):
     print("Error: {}".format(reason))
